72e2c444d3a3208b447785cf275a79d9bd530a97
Momchil Marinov <momchil@mail.com>
2022-11-21 16:00:22 +0100

    offline

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 const divInstall = document.getElementById('installContainer');
 const butInstall = document.getElementById('butInstall');
 
-/* Put code here */
-
-
-
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js');
+  navigator.serviceWorker.register('service-worker.js');
 }
 
 /**
